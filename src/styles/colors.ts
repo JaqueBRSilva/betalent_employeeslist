eddd0633b197600c059e2ef1cbbd2a94f206bfe1
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2025-02-13 16:15:00 -0300

    feat: add constantes de cores e fontes

---
+++ src/styles/colors.ts
@@ -0,0 +1,10 @@
+export const COLORS = {
+    BLUE_PRIMARY: '#0500FF',
+    BLUE_10: '#EDEFFB',
+    BLACK: '#1C1C1C',
+    GRAY_20: '#9E9E9E',
+    GRAY_10: '#DFDFDF',
+    GRAY_05: '#F5F5F5',
+    GRAY_00: '#F0F0F0',
+    WHITE: '#FFFFFF'
+}
---
+++ src/styles/fonts.ts
@@ -0,0 +1,5 @@
+export const FONTS = {
+    HELVETICA: 'Helvetica',
+    HELVETICA_BOLD: 'HelveticaBold',
+    HELVETICA_LIGHT: 'HelveticaLight',
+}
